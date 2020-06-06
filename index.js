0d21f780848fe60204c21763a63ac231c43c97a3
DevFeranmi <40921989+AdewaleAdeniji@users.noreply.github.com>
2020-06-06 22:02:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var element = document.querySelector('.background-item'); //Gets the element
-var sty = getComputedStyle(elem).backgroundImage; //gets the bg image property
-var s = sty.split('"');
+var style = getComputedStyle(element).backgroundImage; //gets the bg image property
+var s = style.split('"');
 var url = s[1];
 window.open(url); //opens the url in new window
